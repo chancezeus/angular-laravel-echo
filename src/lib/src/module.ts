14a95439cf532d9906a811145811b5bd97ed4809
Mark van Beek <flipvb@gmail.com>
2018-03-22 15:17:49 +0100

    feat: Add `forRoot` to NgModule for configuring the module

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -1,10 +1,18 @@
-import {NgModule} from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {EchoService} from "./services/lib.service";
+import {ECHO_CONFIG, EchoConfig, EchoService} from './services/lib.service';
 
 @NgModule({
-    imports: [CommonModule],
-    providers: [EchoService],
+  imports: [CommonModule],
 })
 export class AngularLaravelEchoModule {
+  public static forRoot(config: EchoConfig): ModuleWithProviders {
+    return {
+      ngModule: AngularLaravelEchoModule,
+      providers: [
+        EchoService,
+        {provide: ECHO_CONFIG, useValue: config},
+      ]
+    }
+  }
 }
